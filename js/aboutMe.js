d9df175894ea5ae235ebc777d347080c47c2c1f5
StargazerCatsriel <danbro07@imkatta.no>
2025-08-19 12:40:30 +0200

    Update the Layout, add Sounds

---
+++ js/aboutMe.js
@@ -0,0 +1,9 @@
+const quoteBox = document.getElementById("jevilBox")
+const chaoschaos = document.getElementById("chaosvoice")
+
+quoteBox.addEventListener("click", () => {
+    chaoschaos.play()
+    console.log("CHAOS! CHAOS!")
+
+    // I currently do not have headphones so I cannot check if this works LOL. I hope it does
+})
