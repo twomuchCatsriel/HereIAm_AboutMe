15547c9350f7d67106fbaa1d7f783f1ea8ebec4f
StargazerCatsriel <danbro07@imkatta.no>
2025-08-21 14:06:00 +0200

    Add Yet Another Rick roll easter Egg
    1/1500 chance to change the background to a gif of rick astley.
    
    That's about it.

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -10,9 +10,13 @@ const minowos = document.getElementById("minowos");
 const minosImage = "images/breakingitdown.gif";
 const rickRoll = "images/rickroll.gif";
 
-function checkValue(){
-    let randomValue = Math.floor(Math.random() * 11) // 1 / 10 chance of loading Rick Astley instaed of Minos Prime when the page is loaded
+function checkValue(overrided){
+    let randomRange = (overrided || 11)
+
+    let randomValue = Math.floor(Math.random() * randomRange ) // 1 / 10 chance of loading Rick Astley isntead of Minos Prime when the page is loaded
     let currentState = false;
+
+    console.log(randomValue);
 
     if (randomValue == 1){
         currentState = true;
@@ -31,18 +35,30 @@ function setImage(overrided)
     
     if(state){
         minowos.src = rickRoll;
+        console.log("Never gonna give you up!");
     }
     else{
         minowos.src = minosImage;
+        console.log("Prepare Thyself!");
+    }
+}
+
+function setBackgroundImage(){ //1 / 1500 chance to rickroll the Background of the webpage lol
+    if(checkValue(1501) == 1){
+        document.querySelector("body").style.backgroundImage = "url(images/rickastleyBG.gif)"
+        document.querySelector("body").style.color = "black"
     }
 }
 
 document.addEventListener("DOMContentLoaded", () =>{
     setImage();
+    setBackgroundImage();
 })
 
 // Force Rickroll through command line
 
 function forceRickRoll(){
     setImage(true)
+
+    return "He is giving you up"; 
 }
