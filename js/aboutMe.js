f56e723686ce6c8a979dd11b7aa11176133c9912
StargazerCatsriel <danbro07@imkatta.no>
2025-08-21 11:18:37 +0200

    Add Rickroll Chance on About Me
    Added a 1/100 chance for Rick Astley to replace the gif of Minos Prime dancing at the bottom of the page.
    
    Consider this a rickroll.

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,7 +1,44 @@
-const quoteBox = document.getElementById("jevilBox")
-const chaoschaos = document.getElementById("chaosvoice")
+const quoteBox = document.getElementById("jevilBox");
+const chaoschaos = document.getElementById("chaosvoice");
 
 quoteBox.addEventListener("click", () => {
-    chaoschaos.play()
-    console.log("CHAOS! CHAOS!")
+    chaoschaos.play();
+    console.log("CHAOS! CHAOS!");
+})
+
+const minowos = document.getElementById("minowos");
+const minosImage = "images/breakingitdown.gif";
+const rickRoll = "images/rickroll.gif";
+
+function checkValue(){
+    let randomValue = Math.floor(Math.random() * 101) // 1 / 100 chance of loading Rick Astley instaed of Minos Prime when the page is loaded
+    let currentState = false;
+
+    console.log(randomValue);
+    
+    if (randomValue == 1){
+        currentState = true;
+    } 
+    else
+    {
+        currentState = false;
+    }
+
+    return currentState;
+}
+
+function setImage()
+{
+    let state = checkValue()
+    
+    if(state){
+        minowos.src = rickRoll;
+    }
+    else{
+        minowos.src = minosImage;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    setImage();
 })
